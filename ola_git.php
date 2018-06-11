3d53828720a6f79d780d69291ef484cd75729b5d
Luis Paulo <lpauloss2013@gmail.com>
2018-06-11 19:10:59 -0300

    Primeira Versão

---
+++ ola_git.php
@@ -0,0 +1,11 @@
+1. Selecione o local
+
+cd c:/projeto_integrador
+
+2. Inicializar um repositório git init
+
+3. Setar as minhas configurações
+
+$ git config --global user.name "Luis Paulo"
+
+$ git config --global user.email "lpauloss2013@gmail.com"
